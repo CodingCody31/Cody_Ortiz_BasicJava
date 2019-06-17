9bcc832b4ec3d1abe09dc64865c6060b97fee478
CodingCody31 <50347055+CodingCody31@users.noreply.github.com>
2019-06-17 11:51:00 -0500

    Add files via upload

---
+++ BasicJava/BasicJavaTest.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class BasicJavaTest{
+    public static void main(String[] args){
+        BasicJava iD  = new BasicJava();
+        
+        //iD.printNum(1,255);
+
+        
+        //iD.printOdd(1,255);
+
+        //iD.printSum(1,255);
+
+        /*int[] x = {1,3,5,7,9,13};
+        iD.printArr(x);*/
+
+        /*int[] x = {1,-3,14,0};
+        System.out.println(iD.printMax(x));*/
+
+        /*int[] x = {2,4,5,9};
+        System.out.println(iD.printAvg(x));*/
+
+        //System.out.println(iD.oddArr(1,255));
+
+        /*int[] x = {1,3,5,7};
+        int y = 3;
+        System.out.println(iD.greaterY(x,y));*/
+
+        /*int[] x = {1,5,10,-2};
+        System.out.println(Arrays.toString(iD.squareArr(x)));*/
+
+        /*int[] x = {1,5,2,-10};
+        System.out.println(Arrays.toString(iD.elimNegative(x)));*/
+        
+        /*int[] x = {1,5,10,-2};
+        System.out.println(iD.maxminAvg(x));*/
+
+        int[] x = {1,5,7,10,-2};
+        System.out.println(Arrays.toString(iD.shiftValues(x)));
+    }
+}
